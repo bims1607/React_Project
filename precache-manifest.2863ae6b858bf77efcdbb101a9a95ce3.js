947aa5bb68d9d4423616c5c50c4b05303d18c092
bimal suthar <bimal.suthar1607@gmail.com>
2022-05-27 23:21:43 -0400

    Updates

---
+++ precache-manifest.2863ae6b858bf77efcdbb101a9a95ce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d81e65d7daaa154da25",
+    "url": "/React_Project/static/css/main.a77b677a.chunk.css"
+  },
+  {
+    "revision": "4d81e65d7daaa154da25",
+    "url": "/React_Project/static/js/main.4d81e65d.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/React_Project/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "f7bddeba07740716cec4",
+    "url": "/React_Project/static/css/2.f97f2ce7.chunk.css"
+  },
+  {
+    "revision": "f7bddeba07740716cec4",
+    "url": "/React_Project/static/js/2.f7bddeba.chunk.js"
+  },
+  {
+    "revision": "d46073394ce423974e90",
+    "url": "/React_Project/static/js/runtime~main.d4607339.js"
+  },
+  {
+    "revision": "50d3d73d010728a23d67333d9f7dc001",
+    "url": "/React_Project/index.html"
+  }
+];
